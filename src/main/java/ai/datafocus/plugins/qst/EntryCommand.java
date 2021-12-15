34622197245ec3b4491450e6cf4cb2e7c5e0a5c6
jianglibo <jianglibo@gmail.com>
2021-12-15 14:17:19 +0200

    add verbose option

--- src/main/java/ai/datafocus/plugins/qst/EntryCommand.java
+++ src/main/java/ai/datafocus/plugins/qst/EntryCommand.java
@@ -7,12 +7,31 @@ import io.quarkus.picocli.runtime.annotations.TopCommand;
 import javax.inject.Inject;
 import lombok.Getter;
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.ScopeType;
 
 @TopCommand
 @CommandLine.Command(
     mixinStandardHelpOptions = true,
     subcommands = {StdioDataCommand.class, RedisDataCommand.class, KuduMainCommand.class})
 public class EntryCommand {
+
+  private boolean[] verbosity;
+
+  public int verbosity() {
+    return verbosity.length;
+  }
+
+  @Option(
+      names = {"-v", "--verbose"},
+      scope = ScopeType.INHERIT,
+      description = {
+        "Specify multiple -v options to increase verbosity.",
+        "For example, `-v -v -v` or `-vvv`"
+      })
+  public void setVerbose(boolean[] verbosity) {
+    this.verbosity = verbosity;
+  }
 
   @Getter @Inject MyConfig myconfig;
 }
--- src/main/java/ai/datafocus/plugins/qst/commands/KuduCommand/KuduMainCommand.java
+++ src/main/java/ai/datafocus/plugins/qst/commands/KuduCommand/KuduMainCommand.java
@@ -1,12 +1,12 @@
 package ai.datafocus.plugins.qst.commands.KuduCommand;
 
-import ai.datafocus.plugins.qst.MyConfig;
-import ai.datafocus.plugins.qst.util.AppUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import ai.datafocus.plugins.qst.EntryCommand;
+import io.quarkus.logging.Log;
 import javax.inject.Inject;
 import org.apache.kudu.client.KuduClient;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.ParentCommand;
 
 /** This command doesn't need to know the instanceId. */
 @Command(
@@ -15,15 +15,16 @@ import picocli.CommandLine.Command;
     subcommands = {KuduCreateTable.class, KuduRecordCount.class})
 public class KuduMainCommand {
 
-  @Inject ObjectMapper mapper;
-  @Inject AppUtil appUtil;
-  @Inject MyConfig myconfig;
+  @ParentCommand EntryCommand parent;
 
   @Inject
   @ConfigProperty(name = "kudu.master")
   private String kuduMaster;
 
   public KuduClient kuduClient() {
+    if (parent.verbosity() > 0) {
+      Log.infof("kuduMaster: %s", kuduMaster);
+    }
     KuduClient kudu = new KuduClient.KuduClientBuilder(kuduMaster).build();
     return kudu;
   }
