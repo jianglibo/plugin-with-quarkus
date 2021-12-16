f76da42972e2ed528c3bc5b9686287095a6ccc81
jianglibo <jianglibo@gmail.com>
2021-12-16 15:49:18 +0200

    add warning.

---
+++ .env.tpl
@@ -0,0 +1,3 @@
+kudu.master=192.168.0.22:7051
+quarkus.log.level=WARN
+quarkus.log.console.color=true
--- src/main/java/ai/datafocus/plugins/qst/EntryCommand.java
+++ src/main/java/ai/datafocus/plugins/qst/EntryCommand.java
@@ -5,9 +5,14 @@ import ai.datafocus.plugins.qst.commands.RedisDataCommand;
 import ai.datafocus.plugins.qst.commands.StdioDataCommand;
 import ai.datafocus.plugins.qst.commands.hasura.HasuraCommand;
 import ai.datafocus.plugins.qst.commands.kuducommand.KuduTableCommand;
+import io.quarkus.logging.Log;
 import io.quarkus.picocli.runtime.annotations.TopCommand;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import lombok.Getter;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.ScopeType;
@@ -28,6 +33,10 @@ public class EntryCommand {
 
   @Getter @Inject MyConfig myconfig;
 
+  @Inject
+  @ConfigProperty(name = "hasura.fixture-dir")
+  Path fixtureDir;
+
   public int verbosity() {
     return verbosity == null ? 0 : verbosity.length;
   }
@@ -41,5 +50,26 @@ public class EntryCommand {
       })
   public void setVerbose(boolean[] verbosity) {
     this.verbosity = verbosity;
+  }
+
+  @PostConstruct
+  void after() {
+    Path workdingDir = Path.of(".").toAbsolutePath();
+    Path envFile = workdingDir.resolve(".env");
+    if (!Files.exists(envFile)) {
+      Log.warnf(
+          "There is no '.env' file in the working directory (%s), maybe you should create one or"
+              + " switch to the folder there is a .env file.",
+          workdingDir);
+    }
+
+    if (!Files.exists(fixtureDir)) {
+      Log.warnf(
+          "The hasura graphql fixtures folder does't exists, you should change the setting item"
+              + " 'hasura.fixture-dir' in the '.env' file. Let it points to the fixture directory."
+              + " Usually it is located in the project 'fdatafocus' in the folder"
+              + " 'dcs-plugin-server/fixtrures'",
+          workdingDir);
+    }
   }
 }
